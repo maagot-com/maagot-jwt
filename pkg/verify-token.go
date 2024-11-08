a2f3e15b4411334ef29504ee571878e42a9772b4
emmadal <emmanueldalougou@gmail.com>
2024-11-08 17:15:08 +0000

    fix: change signin method for verification

--- pkg/verify-token.go
+++ pkg/verify-token.go
@@ -12,7 +12,7 @@ func VerifyToken(tokenString string) (uint, error) {
 	secretKey := []byte(os.Getenv("JWT_KEY"))
 	// verify the signature of the token
 	parsedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		_, ok := token.Method.(*jwt.SigningMethodRSA)
 		if !ok {
 			return nil, errors.New("Invalid signature")
 		}
