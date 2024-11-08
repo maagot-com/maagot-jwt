40706dae4237b9c6e6e590ff028c51e9ad6372ca
emmadal <emmanueldalougou@gmail.com>
2024-11-08 17:06:11 +0000

    chore: config jwt module

---
+++ pkg/create-token.go
@@ -0,0 +1,18 @@
+package pkg
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// GenerateToken generate a valid jwt token for 5 minutes
+func GenerateToken(userID uint) (string, error) {
+	secretKey := []byte(os.Getenv("JWT_KEY"))
+	token := jwt.NewWithClaims(jwt.SigningMethodRS512, jwt.MapClaims{
+		"userID": userID,
+		"exp":    time.Now().Add(5 * time.Minute).Unix(), // expire in 5 minutes
+	})
+	return token.SignedString(secretKey)
+}
