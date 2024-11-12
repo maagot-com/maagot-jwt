9afde166456511bf3792074bd0f75514c8f349b1
emmadal <emmanueldalougou@gmail.com>
2024-11-12 13:09:25 +0000

    chore: change jwt verfication

--- pkg/create-token.go
+++ pkg/create-token.go
@@ -1,18 +1,16 @@
 package pkg
 
 import (
-	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-// GenerateToken generate a valid jwt token for 5 minutes
-func GenerateToken(userID uint) (string, error) {
-	secretKey := []byte(os.Getenv("JWT_KEY"))
+// GenerateToken generate a valid jwt token for 10 minutes
+func GenerateToken(userID uint, secretKey []byte) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
 		"userID": userID,
-		"exp":    time.Now().Add(5 * time.Minute).Unix(), // expire in 5 minutes
+		"exp":    time.Now().Add(10 * time.Minute).Unix(), // expire in 10 minutes
 	})
 	return token.SignedString(secretKey)
 }
